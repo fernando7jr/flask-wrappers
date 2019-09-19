935243852e47092852eb69387da80eddbba449cc
Fernando Roberto Setti Junior <fernando.settijunior@gmail.com>
2019-09-19 08:34:44 -0300

    Fixed setup script

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name = 'flask_wrappers',
-    packages = ['flask_wrappers'],
-    version = 'v1.0.1',  # Ideally should be same as your GitHub release tag varsion
-    description = 'Decorators to facilitate flask enpoints implementation',
+    name='flask_wrappers',
+    packages=['flask_wrappers'],
+    version='1.0.2',
+    description='Decorators to facilitate flask enpoints implementation',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author = 'metadeta96',
-    author_email = 'metadeta96@gmail.com',
-    url = 'https://github.com/metadeta96/flask-wrappers',
-    download_url = 'https://github.com/metadeta96/flask-wrappers',
-    keywords = ['flask', 'decorator', 'route', 'wrapper'],
-    classifiers = [
+    author='metadeta96',
+    author_email='fernando.settijunior@gmail.com',
+    url='https://github.com/metadeta96/flask-wrappers',
+    download_url='https://github.com/metadeta96/flask-wrappers',
+    keywords=['flask', 'decorator', 'route', 'wrapper'],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3'
 )
